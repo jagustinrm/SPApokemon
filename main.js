b0b53ce425590559e66cf788fdb31796623d6689
Agustín <jagustin.rm@gmail.com>
2024-05-20 22:58:23 -0300

    tarjetaIndividual

--- main.js
+++ main.js
@@ -1,57 +1,54 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    
-        const links = document.querySelectorAll(".menu__item");
-        links.forEach(link => {
-            link.addEventListener('click', function(event) {
-                event.preventDefault();
-                const url = this.getAttribute('value');
-                fetchPage(url);
-            });
+    const links = document.querySelectorAll(".menu__item");
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const url = this.getAttribute('data-value');
+            fetchPage(url);
         });
     });
-
-    
-    
- 
+});
 
 
-    const currentUrl = window.location.pathname;
-    if (currentUrl === '/' || currentUrl === '/index') {
+// REDIRECCIONAR A HOME
+const currentUrl = window.location.pathname;
+if (currentUrl === '/' || currentUrl === '/index') {
         fetchPage('home');
-    }
+}
 
-    function fetchPage(url) {
-        fetch(`pages/${url}.html`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('La solicitud no fue exitosa');
+// FUNCIÓN DEL DESPLIEGUE DEL MENÚ
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show-menu');
+}
+
+function fetchPage(url) {
+    fetch(`pages/${url}.html`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('La solicitud no fue exitosa');
+            }
+            return response.text();
+        })
+        .then(html => {
+            container.innerHTML = html;
+            document.title = url;
+            
+            const scripts = container.querySelectorAll('script');
+            scripts.forEach(script => {
+                if (script.src) {
+                    const newScript = document.createElement('script');
+                    newScript.src = script.src;
+                    document.head.appendChild(newScript);
+                } else {
+                    
+                    eval(script.innerText);
                 }
-                return response.text();
-            })
-            .then(html => {
-                container.innerHTML = html;
-                document.title = url;
-                
-                const scripts = container.querySelectorAll('script');
-                scripts.forEach(script => {
-                    if (script.src) {
-                        const newScript = document.createElement('script');
-                        newScript.src = script.src;
-                        document.head.appendChild(newScript);
-                    } else {
-                        
-                        eval(script.innerText);
-                    }
-                });
-            })
-            .catch(error => {
-                console.error('Error al obtener la página:', error)
             });
-
-          
-    }
-
-    function toggleMenu() {
-        const menu = document.querySelector('.menu');
-        menu.classList.toggle('show-menu');
+        })
+        .catch(error => {
+            console.error('Error al obtener la página:', error)
+        });       
     }
--- scripts/home.js
+++ scripts/home.js
@@ -2,10 +2,6 @@ function inicializarPaginaHome() {
 const paginationContainer = document.getElementById('pagination');
 
 
-if (currentUrl === '/' || currentUrl === '/index') {
-    loadPokemons(0); 
-}
-
 paginationContainer.addEventListener('click', function(event) {
     const clickedElement = event.target;
 
@@ -33,6 +29,57 @@ function capitalize(text) {
   }
 
 
+ function  handlePokemonClick ({pokemon, mainContent}) {
+    
+    mainContent.innerHTML = ""
+    const number = pokemon.url.match(/\/(\d+)\/$/)[1];
+    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('La solicitud no fue exitosa');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const pokemonImgUrl = data.sprites.front_default;
+        if (pokemonImgUrl) {
+            const contenedor = document.createElement('div');
+            const listaHabilidades = document.createElement('select');
+            data.moves.forEach( (movimiento) => {
+                habilidad = document.createElement('option')
+                habilidad.value = `${movimiento.move.name}`
+                habilidad.textContent = `${movimiento.move.name}`
+                listaHabilidades.appendChild(habilidad)
+            }
+
+            )
+            // const habilidad = document.createElement('li');
+            // habilidad.textContent = `${data.moves[0].move.name}`;
+
+            const pokemonImg = document.createElement('img');
+            const nombre = document.createElement('h2');
+            contenedor.classList.add("pokemonIndividual")
+            pokemonImg.src = pokemonImgUrl;
+
+            
+            const nombrePokemon = capitalize(pokemon.name)
+            nombre.textContent = `${nombrePokemon} #${number}`;
+
+            contenedor.appendChild(nombre)
+            contenedor.appendChild(pokemonImg);
+            contenedor.appendChild(listaHabilidades)
+            mainContent.appendChild(contenedor)
+
+            console.log(data)
+
+        } else {
+            console.error('No se encontró la imagen del Pokémon');
+        }
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
+
+ }
+
 function loadPokemons(offset) {
     
     fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
@@ -47,8 +94,11 @@ function loadPokemons(offset) {
             const pokemonItem = document.createElement('div')
             pokemonItem.classList.add('pokemon-item')
             const nombrePokemon = capitalize(pokemon.name)
-            pokemonItem.textContent = nombrePokemon + " #" + pokemon.url.match(/\/(\d+)\/$/)[1]
+            const pokemonNumber = pokemon.url.match(/\/(\d+)\/$/)[1];
+            pokemonItem.textContent = `${nombrePokemon} #${pokemonNumber}`;
             loadPokemonImage(pokemon.url, pokemonItem); 
+            pokemonItem.onclick = () => handlePokemonClick({pokemon, mainContent});
+
             pokemonList.appendChild(pokemonItem);
             console.log(pokemonList)
         });
